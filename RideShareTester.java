f2f6b61751a5944f2975ab18d8a5316676c6845b
Richard White <rwhite@crashwhite.com>
2024-02-08 08:38:09 -0800

    Added Station class

--- RideShareTester.java
+++ RideShareTester.java
@@ -3,6 +3,8 @@ public class RideShareTester
     public static void main(String[] args)
     {
         System.out.println("Testing RideShare!");
+        /* 
+        // First test
         Car c1 = new Car(1, 5);
         Passenger p1 = new Passenger(1, 3);
         System.out.println(c1);
@@ -13,5 +15,17 @@ public class RideShareTester
         c1.move();
         c1.dropoff();
         System.out.println(c1);
+        */
+
+        // Second test
+        Passenger p1 = new Passenger(1, 3);
+        Station s1 = new Station();
+        s1.addWaiting(p1);
+        System.out.println(p1);
+        System.out.println(s1);
+        s1.removeWaiting(p1);
+        System.out.println(s1);
+
+
     }
 }
---
+++ Station.java
@@ -0,0 +1,54 @@
+/**
+ *  The Station class describes a location along 
+ *  the road where 
+ *  a) a non-riding Passenger can wait for a ride, or
+ *  b) a riding Passenger can disembark, 
+ *     hopefully after arriving at their final 
+ *     destination
+ */
+ 
+import java.util.ArrayList;
+
+public class Station
+{
+    ArrayList<Passenger> waiting;
+    private int stationNum;
+    public static int firstNum = 0;
+    
+    public Station()
+    {
+        waiting = new ArrayList<Passenger>();
+        stationNum = firstNum;
+        firstNum++;
+    }
+
+    public int getStationNum()
+    {
+        return stationNum;
+    }
+
+    public void addWaiting(Passenger p)
+    {
+        waiting.add(p);
+    }
+
+    public void removeWaiting(Passenger p)
+    {
+        waiting.remove(p);
+    }
+
+    public String toString()
+    {
+        String result = super.toString();
+        result += "[stationNum=" + stationNum;
+        result += ",waiting=[";
+        for (int i = 0; i < waiting.size(); i++)
+        {
+            if (i >= 1)
+                result += ","; 
+            result += waiting.get(i);
+        }
+        result += "]]";
+        return result;
+    }
+}
